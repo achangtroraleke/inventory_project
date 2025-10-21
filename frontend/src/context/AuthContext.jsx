4ddf205355f4373344d9dbc98d95730212ea851b
Atachart Changtroraleke <atachart.changtroraleke@gmail.com>
2025-10-21 03:21:38 -0400

    fixed the login component

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -38,7 +38,7 @@ export const AuthProvider = ({children}) =>{
           const res = await axiosPublic.post('/register/', formData);
           console.log(res)
           if(res.status === 201){
-            loginUser(formData={'email':res.data.email,'password':res.data.password})
+            loginUser(formData={'email':res.data.email,'password':formData.password})
             
             return res
          
