07f3e9c36dbc0c0fde2c1ff2b505d5a4cd2161ab
Atachart Changtroraleke <atachart.changtroraleke@gmail.com>
2025-10-21 00:19:12 -0400

    updated settings and build.sh

--- inventory/settings.py
+++ inventory/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'https://inventory-ulxc.onrender.com/',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
