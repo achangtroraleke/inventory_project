f26c5bc318b09294d28b1f77a3fc1ee2e48462f2
Atachart Changtroraleke <atachart.changtroraleke@gmail.com>
2025-10-21 01:36:29 -0400

    removed assets from staticfiles_dirs

--- inventory/settings.py
+++ inventory/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # This production code might break development mode, so we check whether we're in DEBUG mode
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'dist', 'assets'),
+    os.path.join(BASE_DIR, 'frontend', 'dist'),
 ]
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
